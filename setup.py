988da25270ba2bba719eece0aa91c00a382ad63e
Ed Suominen <valueprivacy-foss@yahoo.com>
2018-04-20 17:55:21 -0700

    VERSION 0.9.3

--- setup.py
+++ setup.py
@@ -30,61 +30,88 @@ NAME = "sAsync"
 ### Imports and support
 from setuptools import setup
 
-
 ### Define requirements
-required = ['SQLAlchemy>=0.5', 'AsynQueue>=0.9.0']
+required = ['SQLAlchemy>=0.5', 'AsynQueue>=0.9.4']
 
 
 ### Define setup options
-kw = {'version':'0.9.2',
-      'license':'Apache License (2.0)',
-      'platforms':'OS Independent',
+kw = {'version':           "0.9.3",
+      'license':           "Apache License (2.0)",
+      'platforms':         "OS Independent",
 
-      'url':"http://edsuom.com/{}.html".format(NAME),
-      'author':'Edwin A. Suominen',
-      'author_email':"valueprivacy-foss@yahoo.com",
-      'maintainer':'Edwin A. Suominen',
-      'maintainer_email':"valueprivacy-foss@yahoo.com",
-      'install_requires':required,
-      'packages':['sasync'],
+      'url':               "http://edsuom.com/{}.html".format(NAME),
+      'project_urls':      {
+          'GitHub':     "https://github.com/edsuom/{}".format(NAME),
+          'API':        "http://edsuom.com/{}/{}.html".format(
+              NAME, NAME.lower()),
+          },
+      'author':            "Edwin A. Suominen",
+      'author_email':      "foss@edsuom.com",
+      'maintainer':        "Edwin A. Suominen",
+      'maintainer_email':  "foss@edsuom.com",
       
-      'zip_safe':True
-      }
+      'install_requires':  required,
+      'packages':          [
+          'sasync', 'sasync.test',
+      ],
+      'test_suite':        "sasync.test",
+}
 
 kw['keywords'] = [
-    'SQL', 'SQLAlchemy', 'Twisted', 'asynchronous',
+    'SQL', 'SQLAlchemy', 'twisted', 'asynchronous', 'async',
     'persist', 'persistence', 'persistent',
-    'database']
+    'database', 'sqlite', 'mysql']
+
 
 kw['classifiers'] = [
     'Development Status :: 5 - Production/Stable',
-    
-    'Intended Audience :: Developers',
 
+    'Intended Audience :: Developers',
+    
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 2 :: Only',
     'Framework :: Twisted',
-
-    'Programming Language :: Python',
-    'Programming Language :: SQL',
 
     'Topic :: Database',
     'Topic :: Database :: Front-Ends',
+    'Topic :: Database :: Database Engines/Servers',
     'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+]
 
-kw['description'] = " ".join("""
-SQLAlchemy done Asynchronously, with a convenient transacting database
-access broker and persistent dictionaries, and arrays.
-""".split("\n"))
+# You get 77 characters. Use them wisely.
+kw['description'] =\
+"SQLAlchemy done Asynchronously: Twisted-friendly access to an SQL database."
 
-kw['long_description'] = " ".join("""
-An enhancement to the SQLAlchemy package that provides asynchronous,
-deferred-result access via the Twisted framework and an access broker
+kw['long_description'] = """
+An enhancement to the SQLAlchemy_ package that provides asynchronous,
+deferred-result access via the Twisted_ framework and an access broker
 that conveniently manages database access, table setup, and
 transactions. Included are modules for implementing persistent
 dictionaries and three-dimensional arrays.
-""".split("\n"))
+
+sAsync lets you use the use the database transaction core of the
+SQLAlchemy Python SQL toolkit in an asynchronous fashion. In addition
+to an access broker for conveniently managing database access, table
+setup, and transactions under the Twisted framework, it provides
+persistent item-value stores and arrays.
+
+The sAsync package uses a threaded task queue from the AsynQueue_
+package to wrap your SQLAlchemy database access code inside
+asynchronous transactions. At the lowest level, it provides a
+*@transact* decorator for your database-access methods that makes them
+immediately return a Twisted Deferred object.
+
+Python 3 compatiblity is in the works, but not yet supported.
+
+.. _SQLAlchemy: https://www.sqlalchemy.org/
+
+.. _Twisted: https://twistedmatrix.com/trac/
+
+.. _AsynQueue: http://edsuom.com/AsynQueue.html
+
+"""
 
 ### Finally, run the setup
 setup(name=NAME, **kw)
